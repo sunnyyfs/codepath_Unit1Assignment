3e6691e500521ab191646b4693158c79f8916fa2
sunnyyfs <sunny.yf987@gmail.com>
2022-09-12 19:34:04 -0400

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Sunny Yu on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
